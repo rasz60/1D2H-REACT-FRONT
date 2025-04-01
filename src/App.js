40b93ad3e747347313524bd6361ce97f517b288e
jinwoong <rassayzsixt@gmail.com>
2025-04-01 12:49:01 +0900

    [250401] DAY7. REACT FRONT 서버 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isFetched, setIsFetched] = useState(false);
+
+  useEffect(() => {
+    if (!isFetched) {
+      setIsFetched(true);
+      axios
+        .post(
+          "http://localhost:8079/api/auth/login",
+          {
+            userId: "rassayz60",
+            userPwd: "1234",
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        )
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [isFetched]);
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
